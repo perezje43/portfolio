e81634dd9cc49fe4ecfd38f5a47aa2a1b02caa25
Jeremy Perez <perezje43@gmail.com>
2016-05-11 14:56:32 -0700

    moved functions to app.js

--- js/resume.js
+++ js/resume.js
@@ -44,36 +44,3 @@ var workHistory = [
     '<li>Meet business policies and standards, including providing quality product and cash handling.</li>'
   }
 ];
-
-function addWorkHistory(opts){
-  this.company = opts.company;
-  this.companyUrl = opts.companyUrl;
-  this.title = opts.title;
-  this.startDate = opts.startDate;
-  this.endDate = opts.endDate;
-  this.duties = opts.duties;
-}
-
-addWorkHistory.prototype.toHtml = function() {
-  var $newWork = $('.work-history').clone();
-
-  $newWork.find('h1').text(this.company);
-  $newWork.find('a').attr('href', this.companyUrl);
-  $newWork.find('.work-title').text(this.title);
-  $newWork.find('.start-date').text(this.startDate);
-  $newWork.find('.end-date').text(this.endDate);
-  $newWork.find('.work-duties').append(this.duties);
-
-  $newWork.append('<br>');
-  $newWork.removeClass('work-history');
-  return $newWork;
-};
-
-// adds work history into an array
-workHistory.forEach(function(ele) {
-  work.push(new addWorkHistory(ele));
-});
-
-work.forEach(function(a) {
-  $('#work').append(a.toHtml());
-});
